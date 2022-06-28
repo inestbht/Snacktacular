e435612dd0113a2fd193811dfa1f70d2ae118acc
Samuel Pena <inestbht@gmail.com>
2022-06-27 21:27:05 -0500

    SAVED STATE - Ch. 8.3 - Setting Up the Initial Snacktacular User Interface

---
+++ Snacktacular/SpotListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SpotListViewController.swift
+//  Snacktacular
+//
+//  Created by Samuel Pena on 6/27/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+class SpotListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var spots = ["Island Creek Oysters", "El Pelon", "Shake Shack", "Pino's Pizza"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension SpotListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spots.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotTableViewCell
+        cell.nameLabel?.text = spots[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by Samuel Pena on 6/27/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+}
