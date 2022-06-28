c3ab7aee55df4b2696b9f96729b528e255c6629b
Samuel Pena <inestbht@gmail.com>
2022-06-28 00:30:01 -0500

    SAVED STATE - Ch. 8.4 - Color Assets, Navigation + Toolbar Colors, Status Bar Color, Buttons with Images, and Customizing Segmented Controls

---
+++ Snacktacular/SpotDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SpotDetailViewController.swift
+//  Snacktacular
+//
+//  Created by Samuel Pena on 6/27/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+class SpotDetailViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
