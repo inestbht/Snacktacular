b1e654a74626bcb06ede6242bacd7e7968740be4
Samuel Pena <inestbht@gmail.com>
2022-06-28 23:46:52 -0500

    SAVED STATE - Ch. 8.8 - MapKit - Adding a Map to Plot a Selected Place - Snacktacular App

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 import GooglePlaces
+import CoreLocation
+import MapKit
 
 class SpotDetailViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     var spot: Spot!
+    let regionDistance: CLLocationDegrees = 750.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +26,25 @@ class SpotDetailViewController: UIViewController {
         if spot == nil {
             spot = Spot()
         }
+        setupMapView()
         updateUserInterface()
+    }
+    
+    func setupMapView() {
+        let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        mapView.setRegion(region, animated: true)
     }
     
     func updateUserInterface() {
         nameTextField.text = spot.name
         addressTextField.text = spot.address
+        updateMap()
+    }
+    
+    func updateMap() {
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(spot)
+        mapView.setCenter(spot.coordinate, animated: true)
     }
     
     func updateFromInterface() {
@@ -70,12 +87,9 @@ class SpotDetailViewController: UIViewController {
 extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
     // Handle the user's selection.
     func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-        print("Place name: \(place.name)")
-        print("Place ID: \(place.placeID)")
-        print("Place attributions: \(place.attributions)")
         spot.name = place.name ?? "Unknown Place"
         spot.address = place.formattedAddress ?? "Unknown Address"
-        print("Coordinates = \(place.coordinate)")
+        spot.coordinate = place.coordinate
         updateUserInterface()
         dismiss(animated: true, completion: nil)
     }
