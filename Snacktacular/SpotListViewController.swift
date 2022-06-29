d1206b709a46275711908b8038f598ee4cbf7ea8
Samuel Pena <inestbht@gmail.com>
2022-06-29 10:09:56 -0500

    SAVED STATE - Ch. 8.10 - Sorting Arrays of Structs and Classes to Rearrange TableView Cell Data - Snacktacular App

--- Snacktacular/SpotListViewController.swift
+++ Snacktacular/SpotListViewController.swift
@@ -22,9 +22,10 @@ class SpotListViewController: UIViewController {
         configureSegmentedControl()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         spots.loadData {
+            self.sortBasedOnSegmentPressed()
             self.tableView.reloadData()
         }
     }
@@ -46,6 +47,24 @@ class SpotListViewController: UIViewController {
             let selectedIndexPath = tableView.indexPathForSelectedRow!
             destination.spot = spots.spotArray[selectedIndexPath.row]
         }
+    }
+    
+    func sortBasedOnSegmentPressed() {
+        switch sortSegmentedControl.selectedSegmentIndex {
+        case 0: // A-Z
+            spots.spotArray.sort(by: {$0.name < $1.name})
+        case 1: // closest
+            print("TODO")
+        case 2: // averageRating
+            print("TODO")
+        default:
+            print("HEY! You shouldn't have gotten here. Check out the segmented control for an error!")
+        }
+        tableView.reloadData()
+    }
+    
+    @IBAction func sortSegmentPressed(_ sender: UISegmentedControl) {
+        sortBasedOnSegmentPressed()
     }
 }
 
