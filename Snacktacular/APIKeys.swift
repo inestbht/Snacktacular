52c48f596daf91800069ea88b6e021ee6c433212
Samuel Pena <inestbht@gmail.com>
2022-06-28 21:39:29 -0500

    SAVED STATE - Ch. 8.7 - Adding Google Place Autocomplete - Snacktacular App

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Samuel Pena on 6/28/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyD5RRosnSUHtG3B2ntp094uCN4ywLVwvlw"
+}
