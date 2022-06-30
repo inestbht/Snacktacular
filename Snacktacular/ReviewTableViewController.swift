d2a78ddc3d60811d7e3584e224688d9445a94e2f
Samuel Pena <inestbht@gmail.com>
2022-06-30 13:10:14 -0500

    SAVED STATE - Ch. 8.16 - Adding Responsive Star Buttons and Images for Spot Review Ratings - Snacktacular App

--- Snacktacular/ReviewTableViewController.swift
+++ Snacktacular/ReviewTableViewController.swift
@@ -19,9 +19,21 @@ class ReviewTableViewController: UITableViewController {
     @IBOutlet weak var reviewDateLabel: UILabel!
     @IBOutlet weak var reviewTextView: UITextView!
     @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet var starButtonCollection: [UIButton]!
     
     var review: Review!
     var spot: Spot!
+    var rating = 0 {
+        didSet {
+            for starButton in starButtonCollection {
+                let imageName = (starButton.tag < rating ? "star.fill" : "star")
+                starButton.setImage(UIImage(systemName: imageName), for: .normal)
+                starButton.tintColor = (starButton.tag < rating ? .systemRed : .darkText)
+            }
+            print(">>> new Rating \(rating)")
+            review.rating = rating
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +53,7 @@ class ReviewTableViewController: UITableViewController {
         addressLabel.text = spot.address
         reviewTitleField.text = review.title
         reviewTextView.text = review.text
-        // TODO: Update for rating
+        rating = review.rating // will update rating stars on load
     }
     
     func updateFromUserInterface() {
@@ -80,6 +92,10 @@ class ReviewTableViewController: UITableViewController {
                 print("😡 ERROR: Can't unwind segue from Review because of review saving error.")
             }
         }
+    }
+    
+    @IBAction func starButtonPressed(_ sender: UIButton) {
+        rating = sender.tag + 1 // add one, since our tags start at zero
     }
     
 }
